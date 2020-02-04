e1246213213a50705f7db0c2ba14472630cd1acf
IdionKim <ksundong@gmail.com>
2020-02-04 11:20:17 +0900

    Add Scanner class into Race class
    몬스터 레이스를 수행할 때, 입력을 받아줄 Scanner 객체를 생성자에 넣어주었고,
    이를 닫아주는 부분을 terminateGame이라는 메소드를 생성하여 거기에서 닫을 수 있도록 하였습니다.

--- src/main/java/dev/idion/Race.java
+++ src/main/java/dev/idion/Race.java
@@ -1,5 +1,22 @@
 package dev.idion;
 
-public class Race {
+import java.util.Scanner;
 
+public class Race {
+    private static final String PROMPT = "> ";
+    Scanner scanner;
+
+    public Race() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public static void main(String[] args) {
+        Race race = new Race();
+        race.terminateGame();
+    }
+
+    private void terminateGame() {
+        System.out.println("게임을 종료합니다.");
+        scanner.close();
+    }
 }
