aa8415e09bde18d4553b3c8c65c8e4d209ade7cb
IdionKim <ksundong@gmail.com>
2020-02-07 15:49:31 +0900

    Refactor hardcoded string value to constants

--- src/main/java/dev/idion/Race.java
+++ src/main/java/dev/idion/Race.java
@@ -7,6 +7,16 @@ import java.util.Scanner;
 
 public class Race {
     private static final String PROMPT = "> ";
+    private static final String GAME_NAME = "<심심한 몬스터 경주>";
+    private static final String MONSTER_INPUT_MESSAGE = "몬스터는 모두 몇 마리인가요?";
+    private static final String MONSTER_COUNT_STRING = "몬스터의 수";
+    private static final String ATTEMPT_INPUT_MESSAGE = "시도할 횟수는 몇 회인가요?";
+    private static final String ATTEMPT_COUNT_STRING = "시도할 횟수";
+    private static final String CORRECT_INPUT_STRING = "%s를 정확히 입력해주세요.%n%s";
+    private static final String THE_NUMBER_MUST_BE_POSITIVE_VALUE = "숫자는 양수여야 합니다.";
+    private static final String ERROR_MESSAGE = "게임 실행 도중 오류가 발생했습니다.";
+    private static final String RESULT_MESSAGE = "<실행 결과>";
+    private static final String GAME_EXIT_MESSAGE = "게임을 종료합니다.";
     Scanner scanner;
     int attemptCount;
     Monster[] monsters;
@@ -24,7 +34,7 @@ public class Race {
     }
 
     private void readyGame() {
-        System.out.println("<몬스터 경주>");
+        System.out.println(GAME_NAME);
         monsters = new Monster[inputMonsterCount()];
         fillMonsterIntoMonsters();
         inputAttemptCount();
@@ -37,21 +47,21 @@ public class Race {
     }
 
     private int inputMonsterCount() {
-        System.out.println("몬스터는 모두 몇 마리인가요?");
+        System.out.println(MONSTER_INPUT_MESSAGE);
         System.out.print(PROMPT);
         Integer count = null;
         while (count == null) {
-            count = inputIntTypeValue("몬스터의 수");
+            count = inputIntTypeValue(MONSTER_COUNT_STRING);
         }
         return count;
     }
 
     private void inputAttemptCount() {
-        System.out.println("시도할 횟수는 몇 회인가요?");
+        System.out.println(ATTEMPT_INPUT_MESSAGE);
         System.out.print(PROMPT);
         Integer count = null;
         while (count == null) {
-            count = inputIntTypeValue("시도할 횟수");
+            count = inputIntTypeValue(ATTEMPT_COUNT_STRING);
         }
         attemptCount = count;
     }
@@ -62,14 +72,14 @@ public class Race {
             validateInputValue(inputValue);
             return inputValue;
         } catch (NumberFormatException e) {
-            System.out.printf("%s를 정확히 입력해주세요.%n%s", targetErrorMessage, PROMPT);
+            System.out.printf(CORRECT_INPUT_STRING, targetErrorMessage, PROMPT);
         }
         return null;
     }
 
     private void validateInputValue(int count) {
         if (count < 1) {
-            throw new NumberFormatException("숫자는 양수여야 합니다.");
+            throw new NumberFormatException(THE_NUMBER_MUST_BE_POSITIVE_VALUE);
         }
     }
 
@@ -77,7 +87,7 @@ public class Race {
         try {
             randomAttempt();
         } catch (NoSuchAlgorithmException e) {
-            System.out.println("게임 실행 도중 오류가 발생했습니다.");
+            System.out.println(ERROR_MESSAGE);
             terminateGame(126); // Unix "Command invoked cannot execute"
         }
     }
@@ -106,14 +116,14 @@ public class Race {
     }
 
     private void printMonstersMovingDistance() {
-        System.out.println("<실행 결과>");
+        System.out.println(RESULT_MESSAGE);
         for (Monster monster : monsters) {
-            monster.showMovingDistance();
+            System.out.println(monster);
         }
     }
 
     private void terminateGame(int exitStatus) {
-        System.out.println("게임을 종료합니다.");
+        System.out.println(GAME_EXIT_MESSAGE);
         scanner.close();
         System.exit(exitStatus);
     }
